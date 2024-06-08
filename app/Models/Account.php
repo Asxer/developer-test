fcdf79e931b8c04e4b73e7efdd927488421d42a5
Roman <rdubrovin@artel-workshop.com>
2024-06-08 12:01:07 +0200

    Added reverse relationships

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Account extends Model
 {
@@ -17,8 +18,8 @@ class Account extends Model
         return $this->belongsTo(User::class, 'owner_id');
     }
 
-    public function contacts()
+    public function contacts(): HasMany
     {
-
+        return $this->hasMany(Contact::class);
     }
 }
